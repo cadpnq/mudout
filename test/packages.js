8ad4ba99c752eef7fc4f2685122ec078c1f4ca35
Nicky Nickell <pnqcad@gmail.com>
2019-04-23 12:19:43 -0400

    Add tests for the PackageManager and Watcher classes

---
+++ test/packages.js
@@ -0,0 +1,53 @@
+const PackageManager = require('./../engine/packages');
+const Watcher = require('./../engine/watcher');
+
+describe('Packages', function() {
+  before(function() {
+    global.config = {
+      packages: [
+        'foo',
+      ],
+      enableLogging: false
+    };
+
+    global.logger = require('./../engine/logger');
+  });
+
+  beforeEach(function() {
+    global.packages = new PackageManager();
+  });
+
+  describe('PackageManager', function() {
+    it('should have packages in config added when instantiated', function() {
+      assert(global.packages.packages.has('foo'));
+    });
+
+    describe('addPackage()', function() {
+      it('should add a package', function() {
+        global.packages.addPackage('bar');
+        assert(global.packages.packages.has('bar'));
+      });
+
+      it('should add package to all watchers', function() {
+        let watcher = new Watcher();
+        global.packages.addWatcher(watcher);
+        global.packages.addPackage('bar');
+        assert(watcher.packages.has('bar'));
+      });
+    });
+
+    describe('addWatcher()', function() {
+      it('should add a watcher', function() {
+        let watcher = new Watcher();
+        global.packages.addWatcher(watcher);
+        assert(global.packages.watchers.has(watcher));
+      });
+
+      it('should add all packages to watcher', function() {
+        let watcher = new Watcher();
+        global.packages.addWatcher(watcher);
+        assert(watcher.packages.has('foo'));
+      });
+    });
+  });
+});
