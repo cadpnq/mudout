7d6a64c7736d09b1d3fb49d68f81e2ec7c27ec41
Nicky Nickell <pnqcad@gmail.com>
2020-02-23 19:20:32 -0500

    Add template package

---
+++ packages/templates/mixins/template.js
@@ -0,0 +1,44 @@
+let Template = (extend) => {
+  return class Template extends extend {
+    constructor() {
+      super();
+    }
+
+    static initialize(data) {
+      super.initialize(data);
+    }
+
+    static modify(data) {
+      super.modify(data);
+    }
+
+    modify(data) {
+      super.modify(data);
+    }
+
+    static ready() {
+      return super.ready();
+    }
+
+    static new(obj, args) {
+      super.new(obj, args);
+      return obj;
+    }
+
+    static load(obj, data) {
+      super.load(obj, data);
+      return obj;
+    }
+
+    save() {
+      return super.save();
+    }
+
+    delete() {
+      super.delete();
+    }
+  }
+};
+
+Template.priority = 0;
+module.exports = Template;
---
+++ packages/templates/systems/template.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'template',
+  priority: -1,
+  interval: Infinity,
+  each: (obj, t) => {
+  }
+};
