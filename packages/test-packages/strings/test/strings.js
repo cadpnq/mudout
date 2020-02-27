b3f0effd7eaa75c643b805e638d2d127930343c0
Nicky Nickell <pnqcad@gmail.com>
2020-02-27 15:24:40 -0500

    Add tests for the string system

---
+++ packages/test-packages/strings/test/strings.js
@@ -0,0 +1,45 @@
+const engine = require('./../../../../engine/engine');
+
+describe('Strings', function() {
+  beforeEach(function() {
+    let config = {
+      packages: [
+        'engine',
+        'test-packages/strings'
+      ],
+      packageRoot: 'packages',
+      enableLogging: false
+    };
+    engine.start(config);
+  });
+
+  afterEach(function() {
+    engine.stop();
+  });
+
+  describe('get', function() {
+    it('should return the value of known strings', function() {
+      assert.equal(global.systems.strings.get('test-string'), 'foobar');
+    });
+
+    it('should return an empty string on unknown ones', function() {
+      assert.equal(global.systems.strings.get('asdfasdf'), '');
+    });
+  });
+
+  describe('hotload', function() {
+    it('should change value when the object is changed', function() {
+      assert.equal(global.systems.strings.get('test-string'), 'foobar');
+
+      global.objects.load('', {
+        type: 'instance',
+        of: 'string',
+        name: 'test-string',
+        instanceData: {
+          value: 'bizbuz'
+      }});
+
+      assert.equal(global.systems.strings.get('test-string'), 'bizbuz');
+    });
+  });
+});
